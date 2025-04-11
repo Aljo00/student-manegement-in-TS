360b0abbf9a607abd7e3e60f9b7cd1e14b2bf3a0
Ajai-3 <ajaipjayan@gmail.com>
2025-04-11 08:57:47 +0530

    Folder set up done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./views/**/*.ejs",
+    "./views/admin/**/*.ejs",
+    "./views/student/**/*.ejs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
